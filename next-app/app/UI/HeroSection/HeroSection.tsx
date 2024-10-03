d502be8d47f74593dfb0f8738b727c67d856bf17
jeli-t <jelitotomek@gmail.com>
2024-10-03 10:08:21 +0200

    add HeroSection

---
+++ next-app/app/UI/HeroSection/HeroSection.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import HeroImg from './hero.png'
+
+export function HeroSection() {
+    return (
+        <div className="hero bg-base-100 min-h-screen">
+            <div className="hero-content justify-between max-w-7xl flex-col md:flex-row-reverse gap-4 md:gap-10 xl:gap-40">
+                <Image
+                    src={HeroImg}
+                    className="w-full md:min-w-xl"
+                    alt= "Jakub Knihinicki"
+                />
+                <div className="pb-10 w-full min-w-fit">
+                    <h1 className="text-center text-3xl sm:text-4xl lg:text-5xl xl:text-6xl font-bold">
+                        Jakub Knihinicki
+                        <br />
+                        <span className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-normal">
+                            Instruktor i zawodnik badmintona
+                        </span>
+                    </h1>
+                    <div className="flex justify-center pt-4 lg:pt-8">
+                        <button className="btn px-8 bg-sky-500 text-white">Kontakt</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import { HeroSection } from "./UI/HeroSection/HeroSection";
 
 export default function Home() {
   return (
-    <></>
+    <HeroSection />
   );
 }
