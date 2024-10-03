4eb032f6eae9cae78d339af37d7ff151340bef67
jeli-t <jelitotomek@gmail.com>
2024-10-03 10:08:37 +0200

    fix Navbar

--- next-app/app/UI/Navbar/Navbar.tsx
+++ next-app/app/UI/Navbar/Navbar.tsx
@@ -1,14 +1,14 @@
 export function Navbar() {
     return (
-        <div className="flex justify-center h-fit">
-            <div className="navbar fixed max-w-6xl px-2 md:px-4 rounded-b-xl bg-sky-500">
+        <div className="flex justify-center h-fit text-white">
+            <div className="navbar fixed z-10 max-w-7xl px-4 rounded-b-xl bg-sky-500">
                 <div className="flex-1">
-                    <a className="text-md md:text-xl">Jakub Knihinicki</a>
+                    <a className="text-md md:text-xl font-bold">Jakub Knihinicki</a>
                 </div>
                 <div className="flex-none">
                     <ul className="menu menu-horizontal px-0 md:px-1">
-                    <li><a className="px-2 md:px-4">O mnie</a></li>
                     <li><a className="px-2 md:px-4">Treningi</a></li>
+                    <li><a className="px-2 md:px-4">O mnie</a></li>
                     <li><a className="px-2 md:px-4">Kontakt</a></li>
                     </ul>
                 </div>
